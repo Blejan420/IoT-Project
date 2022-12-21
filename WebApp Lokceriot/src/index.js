8960a83f8a412eda0de11214333b04d15286081a
Topala Dragos <43844818+Akleleafa@users.noreply.github.com>
2022-12-21 11:59:32 +0100

    Delete WebApp Lokceriot directory

--- WebApp Lokceriot/src/index.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator){
-navigator.serviceWorker.register("service-worker.js").then(registration => {
-    console.log ("Service worker registered");
-}).catch(error => {
-    console.log("Service worker registration failed");
-});
-
-}
-
